bc38838a6d886afe378545f66a224907a72a0070
anirud55 <90978843+anirud55@users.noreply.github.com>
2021-11-19 01:49:57 -0800

    Member Sign up

--- FrontEnd/src/components/MSignup.js
+++ FrontEnd/src/components/MSignup.js
@@ -12,6 +12,30 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useEffect, useState } from "react";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormLabel from '@mui/material/FormLabel';
+import countries from "i18n-iso-countries";
+import enLocale from "i18n-iso-countries/langs/en.json";
+import itLocale from "i18n-iso-countries/langs/it.json";
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepButton from '@mui/material/StepButton';
+import history from "../helpers/history";
+import Header from './Header';
+
+<text>Start your MileagePlus adventure by creating an account. Our MileagePlus loyalty program is free to join and rewards you with miles when you fly and when you enjoy everyday activities and experiences.
+
+</text>
+
 
 function Copyright(props) {
   return (
@@ -28,21 +52,53 @@ function Copyright(props) {
 
 const theme = createTheme();
 
-export default function ESignUp() {
+export default function MSignUp() {
+  const [value, setValue] = React.useState(null);
+
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+    const selectCountryHandler = (value) => setSelectedCountry(value);
+    countries.registerLocale(enLocale);
+    countries.registerLocale(itLocale);
+    const countryObj = countries.getNames("en", { select: "official" });
+  
+    const countryArr = Object.entries(countryObj).map(([key, value]) => {
+      return {
+        label: value,
+        value: key
+      };
+  });
+  
+  const navigateToMInfo=()=>{
+    console.log("Navigate to Next page");
+    history.push("/MemberInfo");
+}
+
+
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
-    console.log({
+    /*console.log({
       email: data.get('email'),
       password: data.get('password'),
-    });
+    });*/
+
+    
+
   };
 
+
   return (
+    
     <ThemeProvider theme={theme}>
+      <Header/>
       <Container component="main" maxWidth="xs">
+        
         <CssBaseline />
+        
         <Box
           sx={{
             marginTop: 8,
@@ -51,15 +107,21 @@ export default function ESignUp() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: '#1976d2' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
+          <Typography component="h1" variant="h4">
+            <b>Member Sign up</b>
           </Typography>
+          <br></br>
+          <Typography component="h1" variant="h5">
+            <b>About Me</b>
+          </Typography>
+
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
+              <FormLabel component="legend">First Name*</FormLabel><br></br>
                 <TextField
                   autoComplete="given-name"
                   name="firstName"
@@ -68,9 +130,11 @@ export default function ESignUp() {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  helperText="(As per passport)"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12}>
+              <FormLabel component="legend">Last Name*</FormLabel><br></br>
                 <TextField
                   required
                   fullWidth
@@ -78,50 +142,73 @@ export default function ESignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  helperText="(As per passport)"
                 />
+                <br></br>
+              </Grid>
+              
+              <Grid item xs={12}>
+                <FormLabel component="legend">Gender*</FormLabel>
+                <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
+                  <FormControlLabel value="female" control={<Radio />} label="Female" />
+                  <FormControlLabel value="male" control={<Radio />} label="Male" />
+                  <FormControlLabel value="other" control={<Radio />} label="Unspecified" />
+                </RadioGroup>
+              </Grid>
+
+              <Grid item xs={12}>
+              <FormLabel component="legend">Date Of Birth*</FormLabel>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="MM/DD/YYYY"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              </LocalizationProvider>
               </Grid>
               <Grid item xs={12}>
+              <FormLabel component="legend">Passport Number:*</FormLabel><br></br>
                 <TextField
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                  id="lastName"
+                  label="Passport Number"
+                  name="lastName"
+                  autoComplete="family-name"
                 />
+                <br></br>
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
+              <FormLabel component="legend">Nationality:*</FormLabel><br></br>
+              <Select
+                    style={{ width: "150px" }}
+                    value={selectedCountry}
+                    
+                    onChange={(e) => selectCountryHandler(e.target.value)}
+                >
+                    {!!countryArr?.length &&
+                    countryArr.map(({ label, value }) => (
+                        <MenuItem key={value} value={value}>
+                        {label}
+                        </MenuItem>
+                    ))}
+                </Select>
+                <br></br>
               </Grid>
             </Grid>
+            <Grid>
             <Button
+              onClick={navigateToMInfo}
               type="submit"
-              fullWidth
+              halfWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              sx={{ mt: 3, mb: 2 }}  
             >
-              Sign Up
+              Next
             </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/EsignIn" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
             </Grid>
           </Box>
         </Box>
