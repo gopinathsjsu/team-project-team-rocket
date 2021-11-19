4ae811dbc3d484253898672b919bda8b460872d6
Sudheendra Katikar <sudhu.katikar@gmail.com>
2021-11-18 22:15:49 -0800

    Booking Mongoose model

---
+++ Backend/models/bookings.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookingSchema = new Schema({
+    user_id: { type: String },
+    flight_id: { type: String },
+    status: { type: String },
+    booking_date: { type: Date }
+},
+    { timestamps: true }
+);
+
+const Booking = mongoose.model(
+    'booking',
+    BookingSchema
+);
+
+module.exports = Booking;
