6a2fe8dad6baf4eb759101ec20ee3dd65321ea06
Sudheendra Katikar <sudhu.katikar@gmail.com>
2021-12-01 16:12:21 -0800

    rocket status, checkin components

---
+++ frontend-ng/src/app/components/rocket-status/rocket-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rocket-status',
+  templateUrl: './rocket-status.component.html',
+  styleUrls: ['./rocket-status.component.css']
+})
+export class RocketStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
